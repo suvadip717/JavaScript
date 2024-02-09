70447a24d7a042777782ea41605478849fe56999
suvadip717 <suvadipdas717@gmail.com>
2024-02-09 11:03:47 +0530

    Datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Akash")
+
+let name = "Ajoy"  //String
+let age = 42  //Number
+let isLoggedIn =false //boolean
+
+// number => 2 to power 53
+// bigint
+// string => "",''
+// boolean => true,false
+// null => standalone value
+// undefind =>
+// symbol => unique
+// object
+
+console.log(typeof "Akash") // string
+console.log(typeof 12) // number
+console.log(typeof null)  // object
+console.log(typeof undefined) //undefined
