c105e0bc81bfd258fcbbd4444fcab6de1d28b05f
suvadip717 <suvadipdas717@gmail.com>
2024-02-09 12:34:34 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,3 +39,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+//************************** Operation ********************************
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue) // -3
+
+// console.log(2+2) // 4
+// console.log(2-2) // 0
+// console.log(2*5) // 10
+// console.log(2/2) // 1
+// console.log(7%2) // 1
+
+let str1 = "Good"
+let str2 = " Morning"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2) // 12
+console.log(1+"2") // 12
+console.log(1+"2"+2) // 122
+console.log(1+2+"2") // 32
+
+console.log(3+4*5%3) // 5
+
+console.log(+true) // 1
+console.log(+"") // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// ++gameCounter
+gameCounter++
+console.log(gameCounter) // 101
