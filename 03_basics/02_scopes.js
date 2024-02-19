0f1e3618949cfcba17d8445339be773ab891a194
suvadip717 <suvadipdas717@gmail.com>
2024-02-19 14:55:46 +0530

    Scope level and Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,3 +13,43 @@ if (true) {
 console.log(a)  // 300
 // console.log(b) // b is not defined
 console.log(c) // 50
+
+// Nested Scope
+function one() {
+    const username = "Akash"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)   // Akash
+    }
+    // console.log(website)     // website is not defined
+    two()
+}
+
+one()
+
+// Nested Scope in if-else
+
+if (true) {
+    const username = "Akash"
+    if (username === "Akash") {
+        const website = " YouTube"
+        console.log(username + website)   // Akash YouTube
+    }
+    // console.log(website)    // website is not defined
+}
+// console.log(username)   // username is not defined
+
+
+//************************************************** Interesting ******************************************** */
+
+console.log(addOne(5))  //6
+function addOne(num) {
+    return num + 1
+}
+
+// addTwo(7)       // Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+    return num + 2
+}
+console.log(addTwo(7))  // 9
