b42918659531c00b6430c991acf6f0260e295b79
suvadip717 <suvadipdas717@gmail.com>
2024-02-22 23:20:57 +0530

    control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+const isUserloggedIn = true
+
+if(isUserloggedIn){
+console.log("executed") //executed
+}
+
+
+// <,>,<=,>=,==,!=, ===, ===, !==
+
+const temperature = 41
+if(temperature ===4){
+    console.log("less than 50")
+}
+else{
+    console.log("temperature is less than 50") // temperature is less than 50
+}
+
+const score = 200
+
+if(score>100){
+    const power = "fly"
+    console.log(`User power: ${power}`)  // User power: fly
+}
+
+// console.log(`User power: ${power}`)  // power is not defined
