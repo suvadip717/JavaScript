61f56d6b0130d84c52432e898da776dcae5b1c3f
suvadip717 <suvadipdas717@gmail.com>
2024-02-24 00:02:09 +0530

    if-else statement

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,26 +1,54 @@
 // if
-const isUserloggedIn = true
+const isUserloggedIn = true;
 
-if(isUserloggedIn){
-console.log("executed") //executed
+if (isUserloggedIn) {
+  console.log("executed"); //executed
 }
-
 
 // <,>,<=,>=,==,!=, ===, ===, !==
 
-const temperature = 41
-if(temperature ===4){
-    console.log("less than 50")
-}
-else{
-    console.log("temperature is less than 50") // temperature is less than 50
+const temperature = 41;
+if (temperature === 4) {
+  console.log("less than 50");
+} else {
+  console.log("temperature is less than 50"); // temperature is less than 50
 }
 
-const score = 200
+const score = 200;
 
-if(score>100){
-    const power = "fly"
-    console.log(`User power: ${power}`)  // User power: fly
+if (score > 100) {
+  const power = "fly";
+  console.log(`User power: ${power}`); // User power: fly
 }
 
 // console.log(`User power: ${power}`)  // power is not defined
+
+const balance = 1000;
+
+if (balance > 500) console.log("test"), // test
+console.log("test2");   //test2
+
+if(balance<500){
+    console.log("balance is less then 500")
+}else if(balance<750){
+    console.log("balance is less then 750")
+}else if(balance<900){
+    console.log("balance is less then 900")
+}else{
+    console.log("balance is less then 1200")    // balance is less then 1200
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console .log("Allow to buy course")     // Allow to buy course
+}
+if(userLoggedIn && debitCard && 2==3){
+    console .log("Allow to buy course")     
+}
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User logged in")
+}
