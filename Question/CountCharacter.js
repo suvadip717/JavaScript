175743672be662f295b1f826ea9dc964ce2c8426
suvadip717 <suvadipdas717@gmail.com>
2024-03-16 19:17:03 +0530

    Character count

---
+++ Question/CountCharacter.js
@@ -0,0 +1,5 @@
+// ************************************* Count character Number in a String ******************************************
+
+let ster= "hera krishna hara krishna "
+let sterList = ster.split(" ").join("").split("").length
+console.log(sterList)
