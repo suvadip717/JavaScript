78db9e42e5a0603c06d353496a1e36294e32e09b
suvadip717 <suvadipdas717@gmail.com>
2024-03-26 12:18:17 +0530

    check some test

--- 10_OOPs/09_mathpi.js
+++ 10_OOPs/09_mathpi.js
@@ -16,12 +16,12 @@ console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
 Object.defineProperty(chai, "name", {
   writable: false,
-  enumerable: false,
+  enumerable: false,            // check what is the output when "enumerable: true"
 });
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-for (let [key, value] of Object.entries(chai)) {
+for (let [key, value] of Object.entries(chai)) {            // replace 'Math' at the position 'chai'
   if (typeof value !== "function") {
     console.log(`${key} : ${value}`);
   }
